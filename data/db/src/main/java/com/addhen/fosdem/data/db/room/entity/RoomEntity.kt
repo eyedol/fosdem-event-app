c2ad37a206ae56f0882a5bb759da897746bbeace
Henry Addo <henry@addhen.org>
2019-04-23 21:57:42 +0200

    Remove room ID field as the value does come with the API

--- data/db/src/main/java/com/addhen/fosdem/data/db/room/entity/RoomEntity.kt
+++ data/db/src/main/java/com/addhen/fosdem/data/db/room/entity/RoomEntity.kt
@@ -3,8 +3,6 @@ package com.addhen.fosdem.data.db.room.entity
 import androidx.room.ColumnInfo
 
 data class RoomEntity(
-    @ColumnInfo(name = "room_id")
-    val id: Int,
     @ColumnInfo(name = "room_name")
     val name: String,
     @ColumnInfo(name = "room_building")
--- data/model/src/main/java/com/addhen/fosdem/data/model/Room.kt
+++ data/model/src/main/java/com/addhen/fosdem/data/model/Room.kt
@@ -1,7 +1,6 @@
 package com.addhen.fosdem.data.model
 
 data class Room(
-    val id: Int,
     val name: String,
     val building: String,
     val capacity: Int
