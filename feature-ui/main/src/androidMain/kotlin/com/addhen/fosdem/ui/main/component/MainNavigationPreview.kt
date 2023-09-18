f0b812bd36894bc08db0272bbf6b9788d1481079
Henry Addo <henry@addhen.org>
2023-09-18 11:02:55 +0200

    Add preview for bottom navigation

---
+++ feature-ui/main/src/androidMain/kotlin/com/addhen/fosdem/ui/main/component/MainNavigationPreview.kt
@@ -0,0 +1,55 @@
+// Copyright 2023, Addhen Limited and the FOSDEM app project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package com.addhen.fosdem.ui.main.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.addhen.fosdem.compose.common.ui.api.theme.AppTheme
+import com.addhen.fosdem.compose.common.ui.api.theme.MultiThemePreviews
+import com.addhen.fosdem.core.api.i18n.EnAppStrings
+import com.addhen.fosdem.core.api.screens.SessionScreen
+import com.addhen.fosdem.ui.main.buildNavigationItems
+
+@MultiThemePreviews
+@Composable
+fun MainNavigationPreview() {
+  val strings = EnAppStrings
+  val navigationItems = remember(strings) { buildNavigationItems(strings) }
+  AppTheme {
+    Surface {
+      Scaffold(
+        bottomBar = {
+          MainNavigationBar(
+            selectedNavigation = SessionScreen,
+            navigationItems = navigationItems,
+            onNavigationSelected = {},
+            modifier = Modifier.fillMaxWidth(),
+          )
+        },
+        // We let content handle the status bar
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets
+          .exclude(WindowInsets.statusBars),
+        modifier = Modifier,
+      ) { paddingValues ->
+        Row(
+          modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        ) {
+        }
+      }
+    }
+  }
+}
