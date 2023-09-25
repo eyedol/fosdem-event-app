17eec05a694011bc10370a753859f53b08767b9a
Henry Addo <henry@addhen.org>
2023-09-25 19:14:42 +0200

    Add preview for session details content component

---
+++ feature-ui/session-detail/src/androidMain/kotlin/com/addhen/fosdem/ui/session/detail/component/SessionDetailContentPreview.kt
@@ -0,0 +1,32 @@
+// Copyright 2023, Addhen Limited and the FOSDEM app project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package com.addhen.fosdem.ui.session.detail.component
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.addhen.fosdem.compose.common.ui.api.theme.AppTheme
+import com.addhen.fosdem.compose.common.ui.api.theme.MultiThemePreviews
+import com.addhen.fosdem.core.api.i18n.EnAppStrings
+import com.addhen.fosdem.model.api.day1Event
+
+@MultiThemePreviews
+@Composable
+fun SessionDetailContentPreview() {
+  val appString = EnAppStrings
+  val uiState = SessionDetailItemSectionUiState(
+    event = day1Event,
+    dateTitle = "How regulating software for the european market could impact FOSS",
+    placeTitle = "K-Building",
+    trackTitle = "Keynote",
+    readMoreTitle = appString.readMoreLabel,
+  )
+  AppTheme {
+    Surface {
+      SessionDetailContent(
+        uiState = uiState,
+        onLinkClick = {},
+      )
+    }
+  }
+}
