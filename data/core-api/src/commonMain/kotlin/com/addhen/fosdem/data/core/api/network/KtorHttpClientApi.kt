e45559c92e811fd59d21de706bf6f4b092bff884
Henry Addo <henry@addhen.org>
2024-03-02 12:02:39 +0100

    Install HttpCache for ktor and configure timeouts for ktor client

--- data/core-api/src/commonMain/kotlin/com/addhen/fosdem/data/core/api/network/KtorHttpClientApi.kt
+++ data/core-api/src/commonMain/kotlin/com/addhen/fosdem/data/core/api/network/KtorHttpClientApi.kt
@@ -8,6 +8,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.plugins.HttpRequestRetry
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -30,6 +32,14 @@ object KtorHttpClientApi {
             else -> false
           }
         }
+      }
+
+      install(HttpCache)
+
+      install(HttpTimeout) {
+        socketTimeoutMillis = 15000
+        requestTimeoutMillis = 15000
+        connectTimeoutMillis = 15000
       }
 
       install(Logging) {
