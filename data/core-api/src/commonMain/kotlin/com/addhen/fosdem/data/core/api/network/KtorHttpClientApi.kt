24eb00afb1cad34d8a4da74f99019cd05cf1182a
Henry Addo <henry@addhen.org>
2024-03-02 11:16:49 +0100

    Add retry support to ktor for certain http status code

--- data/core-api/src/commonMain/kotlin/com/addhen/fosdem/data/core/api/network/KtorHttpClientApi.kt
+++ data/core-api/src/commonMain/kotlin/com/addhen/fosdem/data/core/api/network/KtorHttpClientApi.kt
@@ -3,13 +3,15 @@
 
 package com.addhen.fosdem.data.core.api.network
 
+import co.touchlab.kermit.Logger as KermitLogger
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import co.touchlab.kermit.Logger as KermitLogger
+import io.ktor.http.HttpStatusCode
 
 object KtorHttpClientApi {
 
@@ -19,6 +21,17 @@ object KtorHttpClientApi {
   ): HttpClient where T : HttpClientEngineConfig =
     HttpClient(engineFactory) {
       engine(block)
+
+      install(HttpRequestRetry) {
+        retryIf(5) { _, httpResponse ->
+          when {
+            httpResponse.status.value in 500..599 -> true
+            httpResponse.status == HttpStatusCode.TooManyRequests -> true
+            else -> false
+          }
+        }
+      }
+
       install(Logging) {
         logger = object : Logger {
           override fun log(message: String) {
