9cdf5d44e5b86da148990a3779c0d2f802cafe4a
Henry Addo <henry@addhen.org>
2024-03-02 13:04:45 +0100

    Clean up OkHttp configs

--- data/core-api/src/jvmMain/kotlin/com/addhen/fosdem/data/core/api/di/HttpClientPlatformComponent.jvm.kt
+++ data/core-api/src/jvmMain/kotlin/com/addhen/fosdem/data/core/api/di/HttpClientPlatformComponent.jvm.kt
@@ -7,26 +7,21 @@ import com.addhen.fosdem.core.api.di.ApplicationScope
 import com.addhen.fosdem.data.core.api.network.KtorHttpClientApi
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import java.util.concurrent.TimeUnit
 import me.tatarka.inject.annotations.Provides
-import okhttp3.Interceptor
+import okhttp3.ConnectionPool
 
 actual interface HttpClientPlatformComponent {
 
-  @ApplicationScope
-  @Provides
-  fun provideOkHttpNetworkInterceptors(): List<Interceptor> {
-    return listOf()
-  }
-
   @Provides
   @ApplicationScope
-  fun provideHttpClient(
-    networkInterceptors: List<@JvmSuppressWildcards Interceptor>,
-  ): HttpClient {
+  fun provideHttpClient(): HttpClient {
     return KtorHttpClientApi.create(
       engineFactory = OkHttp,
     ) {
-      networkInterceptors.forEach { addNetworkInterceptor(it) }
+      config {
+        connectionPool(ConnectionPool(10, 2, TimeUnit.MINUTES))
+      }
     }
   }
 }
