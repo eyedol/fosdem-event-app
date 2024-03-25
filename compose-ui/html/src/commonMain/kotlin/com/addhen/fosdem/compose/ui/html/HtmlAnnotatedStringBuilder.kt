6d1d882aba098965691336470e58706952867c66
Henry Addo <henry@addhen.org>
2024-03-24 20:59:41 -0400

    Add support for hypens in urls

--- compose-ui/html/src/commonMain/kotlin/com/addhen/fosdem/compose/ui/html/HtmlAnnotatedStringBuilder.kt
+++ compose-ui/html/src/commonMain/kotlin/com/addhen/fosdem/compose/ui/html/HtmlAnnotatedStringBuilder.kt
@@ -218,7 +218,7 @@ internal class HtmlAnnotatedStringBuilder(
   )
 
   private fun findLinks(content: String): Sequence<MatchResult> {
-    return "(?:https?|ftp):\\/\\/[\\w+&@#\\/%?=~_|!:,.;]*[\\w+&@#\\/%=~_|]".toRegex().findAll(content)
+    return "(?:https?|ftp):\\/\\/[\\w+&@#\\/%?=~_|!:,.;-]*[\\w+&@#\\/%=~_|]".toRegex().findAll(content)
   }
 
   /**
