fe8e35bc1fe010cbe4d7c7cc1f669b9bb937df26
Henry Addo <henry@addhen.org>
2024-04-11 15:52:31 -0400

    Enable strict mode to capture unsafe operations by the app

--- android-app/src/main/kotlin/com/addhen/fosdem/android/app/App.kt
+++ android-app/src/main/kotlin/com/addhen/fosdem/android/app/App.kt
@@ -4,11 +4,50 @@
 package com.addhen.fosdem.android.app
 
 import android.app.Application
+import android.os.Build
+import android.os.StrictMode
 import com.addhen.fosdem.android.app.di.AppComponent
 import com.addhen.fosdem.android.app.di.create
 
 class App : Application() {
   val component: AppComponent by lazy {
     AppComponent.create(this)
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    setupStrictMode()
+  }
+
+  private fun setupStrictMode() {
+    StrictMode.setThreadPolicy(
+      StrictMode.ThreadPolicy.Builder()
+        .detectAll()
+        .penaltyLog()
+        .build(),
+    )
+    StrictMode.setVmPolicy(
+      StrictMode.VmPolicy.Builder()
+        .detectLeakedSqlLiteObjects()
+        .detectActivityLeaks()
+        .detectLeakedClosableObjects()
+        .detectLeakedRegistrationObjects()
+        .detectFileUriExposure()
+        .detectCleartextNetwork()
+        .apply {
+          if (Build.VERSION.SDK_INT >= 26) {
+            detectContentUriWithoutPermission()
+          }
+          if (Build.VERSION.SDK_INT >= 29) {
+            detectCredentialProtectedWhileLocked()
+          }
+          if (Build.VERSION.SDK_INT >= 31) {
+            detectIncorrectContextUse()
+            detectUnsafeIntentLaunch()
+          }
+        }
+        .penaltyLog()
+        .build(),
+    )
   }
 }
