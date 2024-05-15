7be36b99d9091f600f1969a02be6c54ce03aab58
Henry Addo <henry@addhen.org>
2024-05-15 14:07:40 -0400

    Fix unit test due to version number change

--- feature-ui/about/src/jvmTest/kotlin/com/addhen/fosdem/ui/about/AboutPresenterTest.kt
+++ feature-ui/about/src/jvmTest/kotlin/com/addhen/fosdem/ui/about/AboutPresenterTest.kt
@@ -16,39 +16,42 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class AboutPresenterTest {
-
-  private val applicationInfo = ApplicationInfo(
-    packageName = "com.addhen.fosdem",
-    debugBuild = false,
-    Flavor.Prod,
-    versionName = "1.0.0",
-    versionCode = 1,
-  )
+  private val applicationInfo =
+    ApplicationInfo(
+      packageName = "com.addhen.fosdem",
+      debugBuild = false,
+      Flavor.Prod,
+      versionName = "1.0.0",
+      versionCode = 1,
+    )
 
   private val navigator = FakeNavigator(AboutScreen)
 
-  private val sut = AboutPresenter(
-    navigator = navigator,
-    applicationInfo = applicationInfo,
-  )
+  private val sut =
+    AboutPresenter(
+      navigator = navigator,
+      applicationInfo = applicationInfo,
+    )
 
   @Test
-  fun `about screen should display correct version name`() = runTest {
-    val expectedVersionName = "${applicationInfo.versionName}(${applicationInfo.versionCode})"
+  fun `about screen should display correct version name`() =
+    runTest {
+      val expectedVersionName = "${applicationInfo.versionName} (${applicationInfo.versionCode})"
 
-    sut.test {
-      val actualAboutUiState = awaitItem()
-      assertEquals(actualAboutUiState.versionName, expectedVersionName)
+      sut.test {
+        val actualAboutUiState = awaitItem()
+        assertEquals(actualAboutUiState.versionName, expectedVersionName)
+      }
     }
-  }
 
   @Test
   fun `given user navigate to Privacy Policy screen emits event to go to privacy policy screen`() =
     runTest {
       val expectedPPLink = "https://addhen.com/privacy-policy"
-      val expectedAboutItem = AboutUiEvent.GoToAboutItem(
-        AboutItem.PrivacyPolicy(expectedPPLink),
-      )
+      val expectedAboutItem =
+        AboutUiEvent.GoToAboutItem(
+          AboutItem.PrivacyPolicy(expectedPPLink),
+        )
 
       sut.test {
         val actualAboutUiState = awaitItem()
@@ -60,33 +63,36 @@ class AboutPresenterTest {
     }
 
   @Test
-  fun `given user navigate to Licenses screen emits event to go to license screen`() = runTest {
-    val expectedAboutItem = AboutUiEvent.GoToAboutItem(
-      AboutItem.License,
-    )
+  fun `given user navigate to Licenses screen emits event to go to license screen`() =
+    runTest {
+      val expectedAboutItem =
+        AboutUiEvent.GoToAboutItem(
+          AboutItem.License,
+        )
 
-    sut.test {
-      val actualAboutUiState = awaitItem()
+      sut.test {
+        val actualAboutUiState = awaitItem()
 
-      actualAboutUiState.eventSink(expectedAboutItem)
+        actualAboutUiState.eventSink(expectedAboutItem)
 
-      assertEquals(LicensesScreen, navigator.awaitNextScreen())
+        assertEquals(LicensesScreen, navigator.awaitNextScreen())
+      }
     }
-  }
 
   @Test
-  fun `given user navigate to Link Screen emits event to go to link screen`() = runTest {
-    val expectedLink = "https://addhen.com"
-    val expectedAboutItem = AboutUiEvent.GoToLink(expectedLink)
+  fun `given user navigate to Link Screen emits event to go to link screen`() =
+    runTest {
+      val expectedLink = "https://addhen.com"
+      val expectedAboutItem = AboutUiEvent.GoToLink(expectedLink)
 
-    sut.test {
-      val actualAboutUiState = awaitItem()
+      sut.test {
+        val actualAboutUiState = awaitItem()
 
-      actualAboutUiState.eventSink(expectedAboutItem)
+        actualAboutUiState.eventSink(expectedAboutItem)
 
-      assertEquals(UrlScreen(expectedLink), navigator.awaitNextScreen())
+        assertEquals(UrlScreen(expectedLink), navigator.awaitNextScreen())
+      }
     }
-  }
 
   @Test
   fun `given user navigate to go to Session Screen emits event to go to session screen`() =
