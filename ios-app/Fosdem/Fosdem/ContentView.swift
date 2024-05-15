381db126311257d0443e0ed3e9439f63a280790a
Henry Addo <henry@addhen.org>
2024-05-15 13:23:37 -0400

    Add ios app to git repo

---
+++ ios-app/Fosdem/Fosdem/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Fosdem
+//
+//  Created by Henry Addo on 5/7/24.
+//
+
+import SwiftUI
+import FosdemKt
+
+struct ContentView: View {
+    private let component: MainUiControllerComponent
+    
+    init(component: MainUiControllerComponent) {
+        self.component = component
+    }
+    var body: some View {
+        ComposeView(component: self.component)
+            .ignoresSafeArea(.all, edges: .all)
+    }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    private let component: MainUiControllerComponent
+    
+    init(component: MainUiControllerComponent) {
+        self.component = component
+    }
+    
+    func makeUIViewController(context _: Context) -> UIViewController {
+        return component.uiViewControllerFactory()
+    }
+
+    func updateUIViewController(_: UIViewController, context _: Context) {}
+}
---
+++ ios-app/Fosdem/Fosdem/FosdemApp.swift
@@ -0,0 +1,39 @@
+//
+//  FosdemApp.swift
+//  Fosdem
+//
+//  Created by Henry Addo on 5/7/24.
+//
+
+import SwiftUI
+import FosdemKt
+
+@main
+struct FosdemApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            let uiComponent = createMainUiControllerComponent(
+                appComponent: delegate.appComponent
+            )
+            ContentView(component: uiComponent)
+        }
+    }
+}
+
+private func createAppComponent() -> AppComponent {
+    return AppComponent.companion.create()
+}
+
+private func createMainUiControllerComponent(
+    appComponent: AppComponent
+) -> MainUiControllerComponent {
+    return MainUiControllerComponent.companion.create(
+        appComponent: appComponent
+    )
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    lazy var appComponent: AppComponent = createAppComponent()
+}
